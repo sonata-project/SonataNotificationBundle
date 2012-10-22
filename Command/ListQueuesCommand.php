1e864b2d762fe28aa8309e60f7c06ea2aeb2fbbf
Robert Gruendler <r.gruendler@gmail.com>
2012-10-22 22:32:10 +0200

    [WIP] added support for multiple queues/routing keys
    CS and typo fix
    
    Added AMQPBackendDispatcher and tests
    
    moved queues config parameter to top
    fixed test namespaces
    
    AMQPDispatcher shares connection with backends now
    
    fixed test
    
    made queue-backend services private
    
    added QueueDispatcherInterface
    
    added QueueNotFoundException
    removed runtime alias
    
    switched to QueueDispatcherInterface in command
    
    Fix some CS, and small bugs
    
    Implemented default_queue for AMQPBackendDispatcher
    
    implemented default queue
    
    added list-queues command
    
    added multiple_queue docs

---
+++ Command/ListQueuesCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sonata\NotificationBundle\Command;
+
+use Sonata\NotificationBundle\Backend\QueueDispatcherInterface;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+
+class ListQueuesCommand extends ContainerAwareCommand
+{
+    public function configure()
+    {
+        $this->setName('sonata:notification:list-queues');
+        $this->setDescription('List all queues available');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $backend = $this->getContainer()->get('sonata.notification.backend');
+
+        if (!$backend instanceof QueueDispatcherInterface) {
+            $output->writeln('The backend class <info>' . get_class($backend)  . '</info> does not provide multiple queues.');
+            return;
+        }
+
+        $output->writeln('<info>List of queues available</info>');
+        foreach($backend->getQueues() as $queue) {
+            $output->writeln(sprintf('queue: <info>%s</info> - routing_key: <info>%s</info>', $queue['queue'], $queue['routing_key']));
+        }
+    }
+}
---
+++ Tests/Backend/AMQPBackendTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace Sonata\NotificationBundle\Tests\Notification;
+
+use Sonata\NotificationBundle\Backend\AMQPBackendDispatcher;
+
+class AMQPBackendTest extends \PHPUnit_Framework_TestCase
+{
+    protected function getMockQueue($queue, $type, $called = null)
+    {
+        $methods = array('createAndPublish');
+        $args = array(array(), '', 'foo', 'message.type.foo');
+        $mock = $this->getMock('Sonata\NotificationBundle\Backend\AMQPBackend', $methods, $args);
+
+        if ($called !== null) {
+            $mock->expects($called)
+                ->method('createAndPublish')
+            ;
+        }
+
+        return $mock;
+    }
+
+    protected function getDispatcher(array $backends, $queue = 'foo', $key = 'message.type.foo')
+    {
+        $queues = array(array('queue' => $queue, 'routing_key' => $key));
+        $settings = array(
+                'host' => 'foo',
+                'port' => 'port',
+                'user' => 'user',
+                'pass' => 'pass',
+                'vhost' => '/'
+        );
+
+        return new AMQPBackendDispatcher($settings, $queues, 'default', $backends);
+    }
+
+    public function testQueue()
+    {
+        $mock = $this->getMockQueue('foo', 'message.type.foo', $this->once());
+        $mock2 = $this->getMockQueue('bar', 'message.type.foo', $this->never());
+        $backends = array('foo' => $mock, 'bar' => $mock2);
+        $dispatcher = $this->getDispatcher($backends);
+        $dispatcher->createAndPublish('message.type.foo', array());
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testInvalidQueue()
+    {
+        $mock = $this->getMockQueue('foo', 'message.type.bar');
+        $dispatcher = $this->getDispatcher(array('bar' => $mock), 'foo', 'message.type.bar');
+        $dispatcher->createAndPublish('message.type.bar', array());
+    }
+}
