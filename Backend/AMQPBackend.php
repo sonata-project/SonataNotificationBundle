82029feea15bcc43eeb829e386da6f57277e495e
Christian Gripp <core23@users.noreply.github.com>
2016-05-22 13:03:46 +0200

    Removed connection in AMQPBackend (#157)

--- Backend/AMQPBackend.php
+++ Backend/AMQPBackend.php
@@ -38,11 +38,6 @@ class AMQPBackend implements BackendInterface
     protected $queue;
 
     /**
-     * @deprecated since version 2.4 and will be removed in 3.0.
-     */
-    protected $connection;
-
-    /**
      * @var string
      */
     protected $key;
