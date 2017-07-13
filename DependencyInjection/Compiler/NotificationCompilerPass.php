1e9083a63a447e53c5e16dfc924c4a19a328b5d9
KDederichs <k.dederichs@spicymedia.io>
2017-07-13 17:12:41 +0200

    Make usage of deprecated event dispatcher conditional (#248)

--- DependencyInjection/Compiler/NotificationCompilerPass.php
+++ DependencyInjection/Compiler/NotificationCompilerPass.php
@@ -12,9 +12,11 @@
 namespace Sonata\NotificationBundle\DependencyInjection\Compiler;
 
 use Sonata\NotificationBundle\Event\IterateEvent;
+use Symfony\Component\DependencyInjection\Argument\ServiceClosureArgument;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Exception\RuntimeException;
+use Symfony\Component\DependencyInjection\Reference;
 
 class NotificationCompilerPass implements CompilerPassInterface
 {
@@ -48,14 +50,28 @@ class NotificationCompilerPass implements CompilerPassInterface
 
                 $informations[$event['type']][] = $id;
 
-                $definition->addMethodCall(
-                    'addListenerService',
-                    array(
-                        $event['type'],
-                        array($id, 'process'),
-                        $priority,
-                    )
-                );
+                /*
+                 * NEXT_MAJOR: Remove check for ServiceClosureArgument and the addListenerService method call.
+                 */
+                if (class_exists('Symfony\Component\DependencyInjection\Argument\ServiceClosureArgument')) {
+                    $definition->addMethodCall(
+                        'addListener',
+                        array(
+                            $event['type'],
+                            array(new ServiceClosureArgument(new Reference($id)), 'process'),
+                            $priority,
+                        )
+                    );
+                } else {
+                    $definition->addMethodCall(
+                        'addListenerService',
+                        array(
+                            $event['type'],
+                            array($id, 'process'),
+                            $priority,
+                        )
+                    );
+                }
             }
         }
 
