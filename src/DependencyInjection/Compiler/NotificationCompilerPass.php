1b75f2a007baa5f47f2917e1eb3e7896b1605c50
Christian Gripp <core23@users.noreply.github.com>
2017-12-21 08:02:08 +0100

    Autoregister consumer as public service (#298)

--- src/DependencyInjection/Compiler/NotificationCompilerPass.php
+++ src/DependencyInjection/Compiler/NotificationCompilerPass.php
@@ -34,6 +34,9 @@ class NotificationCompilerPass implements CompilerPassInterface
         $informations = [];
 
         foreach ($container->findTaggedServiceIds('sonata.notification.consumer') as $id => $events) {
+            $definition = $container->getDefinition($id);
+            $definition->setPublic(true);
+
             foreach ($events as $event) {
                 $priority = isset($event['priority']) ? $event['priority'] : 0;
 
