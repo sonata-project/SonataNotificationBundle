10c971fe28b0aad90e6d889b52604b0529b2fce7
Grégoire Paris <postmaster@greg0ire.fr>
2019-01-10 23:22:12 +0100

    Apply php 7 cs-fixer rules

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the Sonata Project package.
  *
@@ -27,7 +29,7 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder('sonata_notification');
 
         // Keep compatibility with symfony/config < 4.2
-        if (!\method_exists($treeBuilder, 'getRootNode')) {
+        if (!method_exists($treeBuilder, 'getRootNode')) {
             $rootNode = $treeBuilder->root('sonata_notification')->children();
         } else {
             $rootNode = $treeBuilder->getRootNode()->children();
@@ -191,7 +193,7 @@ EOF;
         $treeBuilder = new TreeBuilder('queues');
 
         // Keep compatibility with symfony/config < 4.2
-        if (!\method_exists($treeBuilder, 'getRootNode')) {
+        if (!method_exists($treeBuilder, 'getRootNode')) {
             $node = $treeBuilder->root('queues');
         } else {
             $node = $treeBuilder->getRootNode();
