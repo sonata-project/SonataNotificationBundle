6a9784de9f72be3ced794a369fbb102cad311a42
Jordi Sala Morales <jordism91@gmail.com>
2020-10-22 12:28:08 +0200

    Remove deprecated phpunit calls (#501)

--- tests/Iterator/IteratorProxyMessageIteratorTest.php
+++ tests/Iterator/IteratorProxyMessageIteratorTest.php
@@ -23,64 +23,15 @@ class IteratorProxyMessageIteratorTest extends TestCase
 {
     public function testIteratorProxiesIteratorMethods(): void
     {
-        $content = [
+        $actualIterator = new \ArrayIterator([
             'foo',
             'bar',
-        ];
-
-        $actualIterator = $this->createMock('Iterator');
-        $this->expectIterator($actualIterator, $content, true);
+        ]);
 
         $proxy = new IteratorProxyMessageIterator($actualIterator);
         foreach ($proxy as $eachKey => $eachEntry) {
             $this->assertNotNull($eachKey);
             $this->assertNotEmpty($eachEntry);
         }
-    }
-
-    /**
-     * @see https://gist.github.com/2852498
-     */
-    public function expectIterator($mock, array $content, $withKey = false, $counter = 0): int
-    {
-        $mock
-            ->expects($this->at($counter))
-            ->method('rewind')
-        ;
-
-        foreach ($content as $key => $value) {
-            $mock
-                ->expects($this->at(++$counter))
-                ->method('valid')
-                ->willReturn(true)
-            ;
-
-            $mock
-                ->expects($this->at(++$counter))
-                ->method('current')
-                ->willReturn($value)
-            ;
-
-            if ($withKey) {
-                $mock
-                    ->expects($this->at(++$counter))
-                    ->method('key')
-                    ->willReturn($key)
-                ;
-            }
-
-            $mock
-                ->expects($this->at(++$counter))
-                ->method('next')
-            ;
-        }
-
-        $mock
-            ->expects($this->at(++$counter))
-            ->method('valid')
-            ->willReturn(false)
-        ;
-
-        return ++$counter;
     }
 }
