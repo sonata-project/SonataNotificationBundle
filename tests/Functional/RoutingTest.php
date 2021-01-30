2ada995654a3f7182bd2f08df107e5f60ba7985b
Fran Moreno <franmomu@gmail.com>
2021-01-30 16:15:06 +0100

    Fix wrong namespace

--- tests/Functional/RoutingTest.php
+++ tests/Functional/RoutingTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Sonata\NotificationBundle\Tests\Functional\Routing;
+namespace Sonata\NotificationBundle\Tests\Functional;
 
 use Nelmio\ApiDocBundle\Annotation\Operation;
 use Sonata\NotificationBundle\Tests\App\AppKernel;
