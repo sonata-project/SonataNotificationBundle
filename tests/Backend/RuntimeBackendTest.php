7b172e51a7b849d64d6a5c8a2a9528592b113f1a
Fran Moreno <franmomu@gmail.com>
2021-01-30 16:15:06 +0100

    Fix wrong namespace

--- tests/Backend/RuntimeBackendTest.php
+++ tests/Backend/RuntimeBackendTest.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Sonata\NotificationBundle\Tests\Notification;
+namespace Sonata\NotificationBundle\Tests\Backend;
 
 use PHPUnit\Framework\TestCase;
 use Sonata\NotificationBundle\Backend\RuntimeBackend;
